8a912ff6beea2ab1cdf202849fd70aef23781e84
Shrinivas <shrinivas4927@gmail.com>
2026-01-13 14:04:23 +0530

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,12 +1,9 @@
 import sys
 
-if len(sys.argv) != 2:
-    print("Usage: python script.py <name>")
-    sys.exit(0)
-
-name = sys.argv[1]
-
-if name == name[::-1]:
-    print(f"{name} is a palindrome")
-else:
-    print(f"{name} is not a palindrome")
+if len(sys.argv) == 2:
+    script_name = sys.argv[0]
+    name = sys.argv[1]
+    if name == name[::-1]:
+        print("is a plaindrome")
+    else: 
+        printJ("not a palindrome")
