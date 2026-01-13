f871ec9d3ed607543bf066ef3ea760a7f3c35e32
Shrinivas <shrinivas4927@gmail.com>
2026-01-13 13:48:06 +0530

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -2,8 +2,9 @@ import sys
 if len(sys.argv) == 2:
   script_name = sys.argv[0]
   name = sys.argv[1]
+  if name == name[::-1]:
+    print(f"{name} is a palindrome")
+  else:
+    print(f"{name} is not a palindrome")
 
-if name == name[::-1]:
-  print(f"{name} is a palindrome")
-else:
-  print(f"{name} is not a palindrome")
+
